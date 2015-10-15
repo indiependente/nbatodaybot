e1a2e66d7bb31660116db0c8476087b551bf4595
Francesco Farina <rockerg991@gmail.com>
2015-10-15 20:25:33 +0200

    Implemented /scores command

--- nbatodaybot.js
+++ nbatodaybot.js
@@ -1,7 +1,10 @@
 // An example for OpenShift platform.
-var TelegramBot = require('node-telegram-bot-api')
+var TelegramBot = require('node-telegram-bot-api'),
+	request = require('request'),
+	moment = require('moment')
 
-var token = '34472327:AAGVb9yOxfABeSrSPiJ69REJZevi41fCgtI'
+var token = require('fs').readFileSync('tokenFile').toString()
+
 // See https://developers.openshift.com/en/node-js-environment-variables.html
 var port = process.env.OPENSHIFT_NODEJS_PORT
 var host = process.env.OPENSHIFT_NODEJS_IP
@@ -10,7 +13,30 @@ var domain = process.env.OPENSHIFT_APP_DNS
 var bot = new TelegramBot(token, {webHook: {port: port, host: host}})
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
 bot.setWebHook(domain+':443/bot'+token)
-bot.on('message', function (msg) {
-	var chatId = msg.chat.id
-	bot.sendMessage(chatId, "I'm alive!")
+
+bot.onText(/\/help/, function (msg)
+{
+	bot.sendMessage(msg.chat.id, 'Hello, I am NBA Today Bot.\nType "/scores" to get the latest results from the NBA championship.')
 })
+
+bot.onText(/\/scores/, function (msg)
+{
+	var chatId = msg.chat.id
+	request('http://nbastaz-indiependente.rhcloud.com/matches?date='+moment().subtract({days:1}).format('MM-DD-YYYY'),
+		function (error, response, body)
+		{
+			var scores = ''
+			for (m in body)
+			{
+				scores += body[m].vteam.name + ' ' + capitalize(body[m].vteam.abbr) + ' ' + \
+				body[m].vteam.finl + ' - ' + body[m].hteam.name + ' ' + capitalize(body[m].hteam.abbr) + ' ' + \
+				body[m].hteam.finl + '\n'
+			}
+			bot.sendMessage(chatId, scores)
+		}
+	)
+})
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
